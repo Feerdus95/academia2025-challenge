cd28e88908c9368994250baff76e1d1d02fedae5
Feerdus95 <ferdus95.fv@gmail.com>
2025-08-21 02:12:27 -0300

    fix: postgres user

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -2,14 +2,8 @@
 -- Crear base de datos si no existe
 CREATE DATABASE IF NOT EXISTS academia2025_prod;
 
--- Crear usuario si no existe
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'api_user') THEN
-      CREATE ROLE api_user WITH LOGIN PASSWORD 'secure_password_123';
-   END IF;
-END
-$$;
+-- Cambiar el propietario de la base de datos a postgres
+ALTER DATABASE academia2025_prod OWNER TO postgres;
 
--- Otorgar permisos
-GRANT ALL PRIVILEGES ON DATABASE academia2025_prod TO api_user;
+-- Otorgar todos los privilegios al usuario postgres
+GRANT ALL PRIVILEGES ON DATABASE academia2025_prod TO postgres;
